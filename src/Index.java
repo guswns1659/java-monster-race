4263050b5838e7ac9b0e9a19e658b76655794628
guswns1659 <guswns1659@gmail.com>
2020-02-04 18:44:44 +0900

    20.02.04) Delete useless files

--- src/Index.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-
-public class Index {
-    public static void main(String[] args) {
-        System.out.println((int)(Math.floor(Math.random() * 10)));
-        System.out.println(new Random().nextInt(10));
-    }
-}
